da9f0e44f5dadd11297f80708f9ca29c9a90984d
sihyan <hsq13965557679@163.com>
2024-05-13 10:02:07 +0800

    Initial commit

---
+++ src/main/java/com/codes/CommonCodesApplication.java
@@ -0,0 +1,13 @@
+package com.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonCodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonCodesApplication.class, args);
+    }
+
+}
